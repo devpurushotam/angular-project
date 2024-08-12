40b5834b5e5e88b86771a9aecdd5f4a47ca77f94
Purushotam <purushotam.kumar@trigyn.com>
2024-08-12 10:53:34 +0530

    --code refactor--

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,8 @@ import { SilentAuthComponent } from './silent-auth/silent-auth.component';
 
 
 const routes: Routes = [
-  { path: 'home', component: CommonTestComponent },
+  // { path: 'Home', component: CommonTestComponent },
+  { path: 'common-test', component: CommonTestComponent },
   { path: 'google-class-room', component: GoogleClassroomComponent },
   { path: 'iframe-test', component: GoogleClassroomIframeComponent },
   { path: 'google-auth', component: GoogleAuthComponent },
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-// import { AuthService } from './services/auth-service-iframe';
-// import { GoogleAuthService } from './services/only-token-generation-part'
 
 @Component({
   selector: 'app-root',
@@ -8,71 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
   navItems = [
-    { name: 'Home', link: '/home' },
+    { name: 'Home', link: '/' },
+    { name: 'Common-test', link: '/common-test' },
     { name: 'G-Classroom', link: '/google-class-room' },
     { name: 'G-Auth', link: '/google-auth' },
     { name: 'G-iframe-Test', link: '/iframe-test' },
     { name: 'G-silent-auth', link: '/silent-auth' }
-
   ];
 
   title = 'my-angular-project';
 
-  iframeUrl = "http://localhost:8080"; // Pointing to local HTML file for testing
-
-
-
   constructor(
-    // private googleAuthService: GoogleAuthService
   ) { }
 
   ngOnInit() {
-    // this.googleAuthService.initGoogleAuth().then(() => {
-    //   console.log('Google API client loaded.');
-    // });
   }
 
   addAttachment() {
-    // const authToken = this.authService.getAuthToken();
-    const iframeData = this.parseIframeUrl(this.iframeUrl);
-    const attachment = {
-      // Define your attachment data here
-    };
-
-    // console.log("authToken", authToken);
-
-    // this.classroomService.addAttachment(iframeData.courseId, iframeData.itemId, attachment, authToken);
   }
-
-  parseIframeUrl(url: string) {
-    const urlParams = new URLSearchParams(new URL(url).search);
-    return {
-      addOnToken: urlParams.get('addOnToken'),
-      courseId: urlParams.get('courseId'),
-      postId: urlParams.get('postId'),
-      itemId: urlParams.get('itemId'),
-      itemType: urlParams.get('itemType'),
-      loginHint: urlParams.get('login_hint')
-    };
-  }
-
-  // signIn() {
-  //   this.authService.isAuthenticated$().subscribe(isAuthenticated => {
-  //     console.log("isAuthenticated", isAuthenticated)
-  //     if (!isAuthenticated) {
-  //       this.authService.signIn();
-  //     }
-  //   });
-  // }
-
-
-  // signIn(): void {
-  //   this.googleAuthService.signIn().then(() => {
-  //     const token = this.googleAuthService.getToken();
-  //     console.log('Access Token:', token);
-  //   });
-  // }
-
 }
